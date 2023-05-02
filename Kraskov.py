bf9b3e4ed39f0b485179e9fee8d0a1593d1f360d
Lorenzo Ausilio <92037674+Lausilio@users.noreply.github.com>
2023-05-02 15:04:21 +0200

    correct formula

--- Kraskov.py
+++ Kraskov.py
@@ -14,10 +14,10 @@ def kraskov_entropy(T, k=2):
     r = distances[:, k]
     a = 0
     for i in r:
-        a += np.log(i + 1e-16)
+        a += np.log(i + 1e-16)/np.log(2)
     a = a*(d/n_samples)
     
     # Compute the entropy using Kraskov's formula
-    entropy = (a + d/2*np.log(np.pi) - scipy.special.gammaln(d/2 + 1) + scipy.special.psi(n_samples) - scipy.special.psi(k))
+    entropy = (a + d/2*np.log(np.pi)/np.log(2) - scipy.special.gammaln(d/2 + 1)/np.log(2) + scipy.special.psi(n_samples) - scipy.special.psi(k))
     
     return entropy
